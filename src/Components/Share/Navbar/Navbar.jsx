590af794f7e2e9833a0f76c01af42091722b0f5f
AhaD <abuahad35188@gmail.com>
2023-10-06 23:30:11 +0600

    navbar complite

--- src/Components/Share/Navbar/Navbar.jsx
+++ src/Components/Share/Navbar/Navbar.jsx
@@ -1,8 +1,70 @@
-import React from 'react'
+import logo from '../../../assets/logo/logo.jpg'
+import { Link } from 'react-router-dom'
+import { FaCartPlus} from "react-icons/fa";
+import './navbar.css'
+ 
 
 const Navbar = () => {
+     const user = true;
+     const navOptions = <>
+     <li><Link className="text-xl" to="/">Home</Link></li>
+    <li><Link className="text-xl" to="/">All-Jewelry</Link></li>
+    
+    {
+      user ? <li><Link className="text-xl" to="/dashboard/dashHome">Dashboard</Link></li>:
+       ''
+    }
+    <li>
+        <Link to="/dashboard/my-class" className="text-xl">
+            
+                <div className="badge bg-[#c7dfdf]">+{   0} <FaCartPlus/></div>
+           
+        </Link>
+    </li>
+     
+    {
+        user ? <>
+            <span className="flex text-xl justify-between items-center">{user?.displayName}
+            <img className="h-7 w-7 mt-2 rounded-full" src={user?.photoURL} alt="" />
+            </span>
+          
+            <button   className="btn btn-ghost">LogOut</button>
+        </> : <>
+            <li><Link className="text-xl" to="/login">Login</Link></li>
+        </>
+    }
+     
+   
+    
+    
+     
+</>
   return (
-    <div>Navbar</div>
+    
+     <div className="navbar mx-auto font-bold  bg-opacity-30 max-w-screen-xl bg-black text-[#e8ecef]">
+     <div className="navbar-start">
+         <div className="dropdown">
+             <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+             </label>
+             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                 {navOptions}
+             </ul>
+         </div>
+         <a className="btn btn-ghost normal-case text-xl"> 
+         <img src={logo} className="h-10 w-10 rounded-full" alt="" />
+          
+         </a>
+     </div>
+     <div className="navbar-center hidden lg:flex">
+         <ul className="menu menu-horizontal px-1">
+             {navOptions}
+         </ul>
+     </div>
+     <div className="navbar-end">
+         <Link to='/classes' className="btn"> Blog</Link>
+     </div>
+ </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,18 @@ import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Router/Router.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
+
   <React.StrictMode>
-      <RouterProvider router={router}>
+      <QueryClientProvider client={queryClient}>
+     <RouterProvider router={router}>
           <App />
-      </RouterProvider>
+     </RouterProvider>
+     </QueryClientProvider>
+     
   </React.StrictMode>,
 )
